1a764db9378179e78000b94d807e07e970dea1f0
Joaquin <axxdderr@gmail.com>
2025-06-30 23:19:00 -0500

    Refactoring Users

--- src/App/users/infrastructure/validators/deleteUserValidator.js
+++ src/App/users/infrastructure/validators/deleteUserValidator.js
@@ -2,7 +2,7 @@ import User from "../../domain/models/User.js";
 
 export default class DeleteUserValidator {
     static async validate(data) {
-      await this.validateUserId(data.userId);
+      await this.validateUserId(data);
     }
     static async validateUserId(userId) {
         const user = await User.findByPk(userId);
